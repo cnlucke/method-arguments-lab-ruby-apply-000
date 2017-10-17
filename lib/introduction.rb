06dd27329ef34cee2a430095026a82f9340ce6c1
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-17 17:26:20 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
 def introduction(name)
   puts "You have a beautiful name, #{name}!"
 end
+
+def introduction_with_language(name, language)
+  puts "#{name}, I just know you're really going to love #{language}!"
